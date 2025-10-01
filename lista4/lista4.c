96f77147895ef74fffc7ce9e39d3fa20e5633a92
MarcoJuino <136519016+MarcoJuino@users.noreply.github.com>
2025-10-01 16:52:07 -0300

    Adiciona parcialmente lista 4

--- lista4/lista4.c
+++ lista4/lista4.c
@@ -35,26 +35,43 @@ void mostrar(){
         printf("%d,",numeros[c]);
     }
     printf("\b}");
-    
+
     printf("\nu: {");
     for(c=0;c<5;c++){
         printf("%d,",indImp[c]);
     }
     printf("\b}");
-    
+
     printf("\nw: {");
     for(c=0;c<5;c++){
         printf("%d,",indPar[c]);
     }
     printf("\b}");
-    
+
 }
 
 int main()
 {
-    guardar();
-    separar();
-    mostrar();
+    int resp = 1;
+    while(resp == 1)
+    {
+        guardar();
+        separar();
+        mostrar();
+
+        do{
+            printf("\nQuer continuar o programa?\nDigite (1) para sim e (2) para nao:\n");
+            scanf("%d", &resp);
+
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+
+            if(resp != 1 && resp != 2)
+            {
+                printf("Erro digite 1 ou 2 como resposta!\n");
+            }
+        }while(resp != 1 && resp != 2);
+    }
     return 0;
 }
 #endif
@@ -97,26 +114,159 @@ void mostrar(){
         printf("%d,",numeros[c]);
     }
     printf("\b}");
-    
+
     printf("\nu: {");
     for(c=0;c<impPosition;c++){
         printf("%d,",indImp[c]);
     }
     printf("\b}");
-    
+
     printf("\nw: {");
     for(c=0;c<parPosition;c++){
         printf("%d,",indPar[c]);
     }
     printf("\b}");
-    
+
 }
 
 int main()
 {
-    guardar();
-    separar();
-    mostrar();
+    int resp = 1;
+    while(resp == 1)
+    {
+        guardar();
+        separar();
+        mostrar();
+
+        do{
+            printf("\nQuer continuar o programa?\nDigite (1) para sim e (2) para nao:\n");
+            scanf("%d", &resp);
+
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+
+            if(resp != 1 && resp != 2)
+            {
+                printf("Erro digite 1 ou 2 como resposta!\n");
+            }
+        }while(resp != 1 && resp != 2);
+    }
+
     return 0;
 }
+#endif
+
+#ifdef ex4
+int vetor1[2][3];
+int vetor2[2][3];
+int vetor3[2][3];
+
+main()
+{
+    int resp = 1;
+    int i,n;
+
+    while(resp == 1)
+    {
+        int aux = 0;
+        while(aux != 2)
+        {
+            for(i=0; i<2; i++)
+            {
+                for(n=0; n<3; n++)
+                {
+                    if(aux == 0)
+                    {
+                        printf("Vetor 1 - Digite o numero de posicao %d da linha %d:\n", n, i);
+                        scanf("%d", &vetor1[i][n]);
+                    }
+                    else
+                    {
+                        printf("Vetor 2 - Digite o numero de posicao %d da linha %d:\n", n, i);
+                        scanf("%d", &vetor2[i][n]);
+                    }
+                }
+            }
+            aux = aux + 1;
+        }
+
+        for(i=0; i<2; i++)
+        {
+            for(n=0; n<3; n++)
+            {
+                vetor3[i][n] = vetor1[i][n] + vetor2[i][n];
+            }
+        }
+
+        for(i=0; i<2; i++)
+        {
+            for(n=0; n<3; n++)
+            {
+                printf("O resultado da soma dos indices %d e %d e\' %d + %d = %d \n",i, n, vetor1[i][n], vetor2[i][n], vetor3[i][n]);
+            }
+        }
+
+        do{
+            printf("\nQuer continuar o programa?\nDigite (1) para sim e (2) para nao:\n");
+            scanf("%d", &resp);
+
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+
+            if(resp != 1 && resp != 2)
+            {
+                printf("Erro digite 1 ou 2 como resposta!\n");
+            }
+        }while(resp != 1 && resp != 2);
+    }
+
+}
+#endif
+
+#ifdef ex5
+
+int vetor[10];
+
+void main()
+{
+    int resp = 1;
+    int n, i, aux, s;
+
+    while(resp == 1)
+    {
+        for(n=0; n<10; n++)
+        {
+            printf("Digite o numero de posicao %d do vetor:\n", n);
+            scanf("%d", &vetor[n]);
+
+            for(i=-1; i<n; i++)
+            {
+                if(vetor[n] < vetor[i])
+                {
+                    aux = vetor[i];
+                    vetor[i] = vetor[n];
+                    vetor[n] = aux;
+                }
+            }
+        }
+
+        for (s = 0; s < n; s++) {
+            printf("%d ", vetor[s]);
+        }
+
+        do{
+            printf("\nQuer continuar o programa?\nDigite (1) para sim e (2) para nao:\n");
+            scanf("%d", &resp);
+
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+
+            if(resp != 1 && resp != 2)
+            {
+                printf("Erro digite 1 ou 2 como resposta!\n");
+            }
+        }while(resp != 1 && resp != 2);
+    }
+}
+
 #endif
